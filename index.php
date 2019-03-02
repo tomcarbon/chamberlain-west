e6fab22e68a4ff1973201582c6f8d2daa81a568a
Tom Carbon <tomcarbon@gmail.com>
2019-03-01 16:48:08 -0800

    Add files via upload
    new version .005 with bootstrap added and UI changes.

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CHAMBERLAIN WEST</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/site.css">
+</head>
+<body>
+
+<div class="container">
+    <div class="header row clearfix">
+        <div class="col-md-12">
+            <div class="col-xs-10 col-sm-9 col-md-8 col-lg-7">
+		<br>
+                <a href="index.php"><img id="logo" src="img/logo.png"/></a>
+            </div>
+        </div>
+    </div>
+	<div class="col-md-12">
+		<div id="cwmainapp"></div>
+	</div>
+</div>
+
+<script src="js/jquery3.3.1.min.js"></script>
+<script src="js/vue.js"></script>
+<script src="js/cwmain.js"></script>
+</body>
+</html>
