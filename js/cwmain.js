5f7277c4ab209a86b4873e4c6a25165fae5c2a5b
Tom Carbon <tomcarbon@gmail.com>
2019-03-26 10:19:14 -0700

    Add files via upload
    fixed super/subscript issues, added June 2018 section to CV

--- js/cwmain.js
+++ js/cwmain.js
@@ -4,7 +4,7 @@ Vue.component('physicsapp', {
                         <div id="physicsapp">
                                 <h1>Physics</h1>
                                 <h2>Selected Physics Papers and Conference Charts</h2>
-                                <table>
+                                <table> 
                                           <tr>
                                             <th>Article Title</th>
                                             <th>Author</th>
@@ -14,7 +14,7 @@ Vue.component('physicsapp', {
                                         <td>
                                         <a href="papers/Fundamentally%20Anisotropic%20Light-Velocity%20at%20the%20Foundation%20of%20Classical%20Physics.pdf"> Fundamentally Anisotropic Light-Velocity at the Foundation of Classical Physics (pdf)</a>
                                         </td>
-                                        <td>  Thomas E. Chamberlain</td>
+                                        <td >  Thomas E. Chamberlain</td>
                                         </tr>
                                         <tr>
                                         <td>
@@ -134,14 +134,16 @@ Vue.component('tec-cv', {
                         <p>In physics his studies over the years focused on conceptualizing relativity and quantum mechanics (to the extent either may be possible). In 2012 he decided to concentrate on basic physics—suspending psychology and economics—with the goal of eventually uniting anisotropic (light-velocity) relativity and quantum theory. Because orthodox relativity rejects anisotropic light-velocity this effort immediately concentrated on special relativity to the neglect of quantum theory. In 2014 special relativity (and therefore general relativity) was found incomplete due to the isotropic c=constant postulate. The resulting paper “Fundamentally anisotropic light-velocity at the foundation of classical physics” was presented at the 2015 June AAAS-PD conference in San Francisco. (Original title changed).</p>
 
                         <h3>Far-Field Gravitation</h3>
-                        <p>On the basis of a belief, from well before 2012, that one-way (near) infinite light-speed was crucially germane to deepening classical physics theory, almost immediately after the San Francisco conference (2015 July) he ascertained that Hubble expansion and Einstein’s same-motion acceleration (1907) are essentially related (in some manner)—inasmuch as equating the Hubble expansion, V=rH, to the similar expansion from S-M acceleration (in the limit V=c) with the latter as a free parameter yielded a=cH=6.3E-10 m/s2, which is within an order of Milgrom’s universal acceleration constant a0=1.2E-10 m/s2.</p>
-                        <p>Another nearly seven months (to 2016 February) were required to place Milgrom’s empirical Deep MOND on a relativistic basis (i.e., within “state-of-art” relativity physics, which extends standard relativity physics to formally recognize anisotropic light-speed—see Rizzi et al. (2008) and the AAAS-PD/San Francisco paper). Two new inductive advances enabled this theoretical advance: (1) conjunction of one-way infinite light-speed and the Hubble expansion to yield the corresponding time dilation; followed by (2) combining this time-dilation with its counterpart emerging from the Schwarzschild solution. The result was the relationship VL2=2(GM/rS)1/2(rHcH0)1/2 – departing from Milgrom’s functionally similar Vf2=(GMa0)1/2 by the Schwarzschild-based radius rS and the Hubble-expansion based rH, which, being equal, cancel out. (Note that VL is not Milgrom’s circular orbit speed Vf but rather the (also invariant) speed emerging from the linearized Lorentz transformation. The factor “2” had been mistakenly overlooked and was inserted as part of the previous revision on March 29, 2017.)</p>
+                        <p>On the basis of a belief, from well before 2012, that one-way (near) infinite light-speed was crucially germane to deepening classical physics theory, almost immediately after the San Francisco conference (2015 July) he ascertained that Hubble expansion and Einstein’s same-motion acceleration (1907) are essentially related (in some manner)—inasmuch as equating the Hubble expansion, V=rH, to the similar expansion from S-M acceleration (in the limit V=c) with the latter as a free parameter yielded <a href="index.php"><img src="img/cv02.png"/></a>, which is within an order of Milgrom’s universal acceleration constant <a href="index.php"><img src="img/cv03.png"/></a>.</p>
+                        <p>Another nearly seven months (to 2016 February) were required to place Milgrom’s empirical Deep MOND on a relativistic basis (i.e., within “state-of-art” relativity physics, which extends standard relativity physics to formally recognize anisotropic light-speed—see Rizzi et al. (2008) and the AAAS-PD/San Francisco paper). Two new inductive advances enabled this theoretical advance: (1) conjunction of one-way infinite light-speed and the Hubble expansion to yield the corresponding time dilation; followed by (2) combining this time-dilation with its counterpart emerging from the Schwarzschild solution. The result was the relationship <a href="index.php"><img src="img/cv04.png"/></a> – departing from Milgrom’s functionally similar <a href="index.php"><img src="img/cv05.png"/></a> by the Schwarzschild-based radius <a href="index.php"><img src="img/cv06.png"/></a> and the Hubble-expansion based <a href="index.php"><img src="img/cv07.png"/></a>, which, being equal, cancel out. (Note that <a href="index.php"><img src="img/cv08.png"/></a> is not Milgrom’s circular orbit speed <a href="index.php"><img src="img/cv09.png"/></a> but rather the (also invariant) speed emerging from the linearized Lorentz transformation. The factor “2” had been mistakenly overlooked and was inserted as part of the previous revision on March 29, 2017.)</p>
                         <p>Further developments over the next few months towards a complete deepening of the Einstein Field Equations were eventually suspended in order to prepare for the rapidly approaching AAAS-PD Conference in San Diego (14-17 June 2016), where the approximate formulation of Milgrom’s expression with comparison against astronomical “far-field” star and gas velocities was presented.</p>
 
                         <h3>Accommodation of Far-Field Gravitation in the Schwarzschild Solution</h3>
-                        <p>The principal task in allowing completion of the paper has been establishing how the inverse-radius dependence of far-field gravitation emerges from the invariant far-field time-dilation d(Del-t')/dt=-(GMcH0)1/2/c2, a continuing task that began with the relation’s origination in 2016 February and was finally achieved in early November. Steady, non-accelerating Hubble expansion was the key permitting </p>
-                        <p>a = d[(GM/rS)(rHcH)]1/2/dr = - ½ (GMcH0)1/2/r  while  d[(GM/rS)(rHcH0)]1/2/dr=0.</p>
-                        <p> (Note that the factor “1/2” was initially overlooked and inserted as part of the previous revision on March 29, 2017. A significant consequence of this correction was a much-improved correspondence or agreement between Milgrom’s a0=1.2E-13 km/s2 and the present cH0/4≈1.6E-13 km/s2.)</p>
+                        <p>The principal task in allowing completion of the paper has been establishing how the inverse-radius dependence of far-field gravitation emerges from the invariant far-field time-dilation <a href="index.php"><img src="img/cv10.png"/></a>, a continuing task that began with the relation’s origination in 2016 February and was finally achieved in early November. Steady, non-accelerating Hubble expansion was the key permitting </p>
+	<div class="col-md-12">
+	<a href="index.php"><img src="img/cv01.png"/></a>
+	</div>
+                        <p> (Note that the factor “1/2” was initially overlooked and inserted as part of the previous revision on March 29, 2017. A significant consequence of this correction was a much-improved correspondence or agreement between Milgrom’s <a href="index.php"><img src="img/cv11.png"/></a> and the present <a href="index.php"><img src="img/cv12.png"/></a>.)</p>
                         <p>Additional concerns delayed the paper, but it was completed soon before traveling to the AAAS-PD conference in Hawaii with presentation on 22 June. The paper may be accessed in the “Selected Physics Papers” of this website.</p>
 
                         <h3>Cosmic Acceleration/Deceleration</h3>
@@ -199,7 +201,7 @@ Vue.component('tec-cv', {
 
                                 <p class="specialp">2015 June: Initiated revision of general relativity to accommodate anisotropic light-velocity.</p>
 
-                                <p class="specialp">2016 February: Developed Milgrom's empirical Vf=(GMa0)1/4 as  theoretical VL=21/2(GMcH0)1/4 within up-to-date relativity physics. (Corrected Vf=(GMcH0)1/4 toVL=21/2(GMcH0)1/4 in the prior revision.)</p>
+                                <p class="specialp">2016 February: Developed Milgrom's empirical <a href="index.php"><img src="img/cv17.png"/></a> as  theoretical <a href="index.php"><img src="img/cv16.png"/></a> within up-to-date relativity physics. (Corrected <a href="index.php"><img src="img/cv15.png"/></a> in the prior revision.)</p>
 
                                 <p class="specialp">2016 June: Letter published in the Financial Times (2 June) recommending conditional basic income (properly done) to grow human capital thereby promoting "the basic economic right, the Right to Life", where the resulting boost to economic growth yields increased tax revenue to keep government finances in balance.  </p>
 
@@ -208,9 +210,9 @@ Vue.component('tec-cv', {
                                 <p class="specialp">2016 November:   Recognized     steady,   non-accelerating   Hubble   expansion    as    the   basis  for   mathematical   derivation   of   far-field  gravity (inserted factor ½ correction in the previous revision)</p>
 
                                 <center>
-                                        <p>               a = d([GM/rS][rHcH])1/2/dr = - ½ (GMcH0)1/2/r,</p>
+                                        <p>             <a href="index.php"><img src="img/cv13.png"/></a> ,</p>
                                         <p>while acknowledging</p>
-                                        <p>               d([GM/rS][rHcH0])1/2/dr = 0,</p>
+                                        <p>               <a href="index.php"><img src="img/cv14.png"/></a>,</p>
                                 </center>
 
                                 <p>thereby promoting a prospective metric for extending the Schwarzschild solution to accommodate inverse-r far-field gravity and clearing the path to completing the paper.</p>
@@ -224,6 +226,7 @@ Vue.component('tec-cv', {
                                 <p class="specialp">2018 March: Hubble expansion acceleration for the empty universe (Ω=0 with dH/dt=0) was presented as a new result at the Texas2017 symposium. Further developments since December that model (partially) offsetting pure cosmic acceleration versus sub-field cosmic deceleration in the local universe (z <~ 0.1) - with comparison versus Sn-1a apparent luminosity residuals - will be given at the APS April Meeting in Columbus. </p>
 
                                 <p class="specialp">2018 June: Progress on the nature of cosmic acceleration/deceleration was presented at the 2018 APS April Meeting in Columbus. Follow-on advances were given at the 2018 AAAS-PD Conference in Pomona, California, with the presentation title "Relativistic Formulation of Cosmic Acceleration vs. Cosmic Deceleration in the Local Universe." The corresponding abstract and charts are available on the Physics Page; completion of the "in progress" paper is expected later this year.</p>
+                                <p class="specialp">2018 November: A more substantive derivation of cosmic acceleration entitled “Derivation of Cosmic Acceleration Given Anisotropic Light-Speed in the Hubble Expansion” yielding <a href="index.php"><img src="img/cv18.png"/></a> to leading order was presented at the annual APS Far West Section meeting (Fullerton) on 19 October, showing good agreement with supernova SNe-Ia acceleration measurements in the local universe, 0.01<z<0.3. The abstract and charts are available on the Physics Page. Work continues on the cosmic acceleration paper with completion pushed into early-to-mid 2019.</p>
                         </div>          `       // do not forget this little tick!
 });
 
@@ -265,13 +268,23 @@ const cwmainapp = new Vue({
 		},
 	},		
 	template: `
-		<div >
-			<div id="theprimary" class="col-xs-12 col-sm-3 col-md-2">
-				<button class="nicebutton0" id="idhome" v-on:click="homebutton">HOME</button>
-				<button class="nicebutton0" id="ideconomics" v-on:click="economicsbutton">ECONOMICS</button>
-				<button class="nicebutton0" id="idphysics" v-on:click="physicsbutton">PHYSICS</button>
+		<div class="col-md-12">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 header row clearfix" id="headerspec">
+				<div class="hidden-xs col-sm-12 col-md-12 col-lg-12">
+					<h1> Chamberlain West</h1>
+				</div>
+				<div class="col-xs-12 hidden-sm hidden-md hidden-lg">
+					<h4> Chamberlain West</h4>
+				</div>
+					<!--a href="index.php"><img src="img/logo.png"/></a-->
+
+				<div class="col-md-12">
+					<button class="nicebutton0" id="idhome" v-on:click="homebutton">HOME</button>
+					<button class="nicebutton0" id="ideconomics" v-on:click="economicsbutton">ECONOMICS</button>
+					<button class="nicebutton0" id="idphysics" v-on:click="physicsbutton">PHYSICS</button>
+				</div>
 			</div>
-			<div class="col-xs-12 col-sm-9 col-md-10" color="black">
+			<div class="col-md-12" color="black">
 				<tec-cv />
 	                        <economicsapp />
 	                        <physicsapp />
